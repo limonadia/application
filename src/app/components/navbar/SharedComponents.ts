0960be194aff30b120f22d3015e9dc275fa85607
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-03-24 17:52:56 +0100

    Finish navbar

--- src/app/components/navbar/SharedComponents.ts
+++ src/app/components/navbar/SharedComponents.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
-import { MatIconModule } from '@angular/material/icon';
 
 const components = [
     NavbarComponent
@@ -10,12 +9,10 @@ const components = [
 @NgModule({
     imports: [
       CommonModule,
-      MatIconModule
     ],
     declarations: components,
     exports: [
       ...components,
-      MatIconModule
     ]
   })
   export class SharedComponentsModule { }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  imageUrl?: string;
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.imageUrl = "https://merrimacshs.eq.edu.au/SiteCollectionImages/news-icons/Kinetic.png";
+    }, 500);
   }
 
 }
