e4089b87c0f45a9335674a0c0664f0dcc789f511
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-03-24 16:22:26 +0100

    Implement tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Scan all HTML and TypeScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
