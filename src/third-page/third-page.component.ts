0163007790f8345e2f247ba5dbc3d3a989150042
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-03-27 15:37:44 +0100

    Finish third-page layout & functionality

--- src/third-page/third-page.component.ts
+++ src/third-page/third-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-third-page',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThirdPageComponent implements OnInit {
 
-  constructor() { }
+  time?: string;
+  name?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  pm?: boolean;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.time = params['time'];
+      this.name = params['name'];
+      this.lastName = params['lastName'];
+      this.email = params['email'];
+      this.phone = params['phone'];
+    });
+
+    if(Number(this.time)<=5 || Number(this.time)==12){
+      this.pm = true;
+    } else {
+      this.pm = false;
+    }
+
+   }
 
   ngOnInit() {
   }
